12feae5cbca100f71651f40b9ced619cc266d10d
chawala1408 <69667870+chawala1408@users.noreply.github.com>
2025-06-16 12:27:44 +0700

    Update test_update.py

--- test_update.py
+++ test_update.py
@@ -4,12 +4,39 @@ from PySide6.QtUiTools import QUiLoader
 import os
 import sys
 import signal
+import requests
+import subprocess , time
 
+GIT_RAW_URL = "https://raw.githubusercontent.com/chawala1408/update/refs/heads/main/test_update.py"
+LOCAL_UPDATE_FILE = "test_update2.py"
+
+def download_update():
+    try:
+        print("⬇️  Downloading latest version of test_update.py...")
+        response = requests.get(GIT_RAW_URL)
+        response.raise_for_status()
+        with open(LOCAL_UPDATE_FILE, "w", encoding="utf-8") as f:
+            f.write(response.text)
+        print("✅ Update downloaded successfully.")
+        return True
+    except Exception as e:
+        print("❌ Failed to download update:", e)
+        return False
+    
 def update():
-    print("Update function called", flush=True)  # <-- สำคัญมาก
-    sys.stdout.flush()  # เพิ่มอีกเผื่อไว้
-    os.kill(os.getpid(), signal.SIGTERM)
-    QCoreApplication.quit()
+    print("Update function called", flush=True)  # <-- สำคัญมากdownload_update()
+    if download_update():
+        print("🚀 Running updated version...")
+        subprocess.Popen(["update.bat"], shell=True)
+        sys.stdout.flush()  # เพิ่มอีกเผื่อไว้
+        time.sleep(1)
+
+        # ปิดตัว
+        QCoreApplication.quit()
+        os.kill(os.getpid(), signal.SIGTERM)
+    else:
+        print("⚠️ Update download failed. Keeping old version.")
+    
 
 app = QApplication([])
 
@@ -40,4 +67,3 @@ if __name__ == "__main__":
     window.show()
     sys.exit(app.exec())
 
-#1233
